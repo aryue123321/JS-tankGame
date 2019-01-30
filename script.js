7fcb89edec6b993beeef8e7a6b14c261994b4f35
Chun Yue Leung <aryue123321@gmail.com>
2019-01-30 10:55:22 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+const btnStart = document.querySelector('.btnStart');
+const gameOverEle = document.getElementById('gameOverEle');
+const container = document.getElementById('container');
+const box = document.querySelector('.box');
+const statusBar = document.getElementById('statusBar');
+const boxCenter = [box.offsetLeft + box.offsetWidth/2, box.offsetTop + box.offsetHeight/2];
+const icons = ['angry', 'basketball-ball', 'baseball-ball','accessible-icon','ambulance','centos'];
+
+console.log(boxCenter);
+
+let player;
+let gamePlay = false;
+let animateGame;
+
+btnStart.addEventListener('click', startGame);
+
+container.addEventListener('mousedown', mouseDown);
+container.addEventListener('mousemove', mouseMove);
+
+function mouseMove(e){
+  let deg = calcAngleDegrees(e);
+  box.style.transform = `rotate(${deg}deg)`;
+  box.style.webkitTransform = `rotate(${deg}deg)`;
+  box.style.mozTransform = `rotate(${deg}deg)`;
+  box.style.msTransform = `rotate(${deg}deg)`;
+  box.style.oTransform = `rotate(${deg}deg)`;
+  box.style.transform = `rotate(${deg}deg)`;
+}
+
+function calcAngleDegrees(e) {
+  return Math.atan2(e.clientX - boxCenter[0], -e.clientY + boxCenter[1]) * 180 / Math.PI;
+}
+
+function mouseDown(e){
+  if (gamePlay){
+    div = document.createElement('div');
+    div.classList.add('fireme');
+    let radian = calcAngleDegrees(e) * Math.PI / 180;
+    div.style.left = `${boxCenter[0]-5}px`;
+    div.style.top = `${boxCenter[1]-5}px`;
+    div.rx = 5 * Math.sin(radian); 
+    div.ry = -5 * Math.cos(radian);
+    // div.style.width = '10px';
+    // div.style.height = '10px';
+    container.appendChild(div);
+  }
+}
+
+function randomInt(range){
+  return Math.floor(Math.random()*range);
+}
+
+function makeOneBadGuy(){
+  const div = document.createElement('div');
+  div.classList.add('baddy', 'fa', `fa-${icons[randomInt(icons.length)]}`);
+  
+  switch(randomInt(0)){
+    case 0:
+      div.style.left = 0 +'px';
+      div.style.top = statusBar.offsetHeight+20+randomInt(container.offsetHeight-statusBar.offsetHeight-50) +'px';
+      div.rx = 3 + randomInt(3);
+      
+  }
+  container.appendChild(div);
+}
+
+function generateBadGuys(num){
+  for (let i = 0; i < num; i++){
+    makeOneBadGuy();
+  }
+}
+
+function startGame(){
+  gamePlay = true;
+  gameOverEle.style.display = 'none';
+  player = {
+    score : 0,
+    lives : 100,
+    barWidth: 100
+  }
+  generateBadGuys(100);
+  animateGame = requestAnimationFrame(playGame);
+}
+
+
+function moveShots(){
+  let shots = document.querySelectorAll('.fireme');
+  shots.forEach(x => {
+    if (x.offsetTop > container.offsetHeight-statusBar.offsetHeight || x.offsetTop < statusBar.offsetHeight+20 || x.offsetLeft < 0 || x.offsetLeft > container.offsetWidth){
+      x.parentNode.removeChild(x);
+    }else{
+      x.style.left = `${x.offsetLeft + x.rx}px`;
+      x.style.top = `${x.offsetTop + x.ry}px`;
+    }
+  });
+}
+
+function playGame(){
+  if (gamePlay){
+    moveShots();
+    animateGame = requestAnimationFrame(playGame);
+  }
+}
